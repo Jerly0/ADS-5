644b4302ea975a057f0e72788b75faf884534194
Jerly0 <87529148+Jerly0@users.noreply.github.com>
2022-04-02 13:20:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,6 +3,33 @@
 #include <map>
 #include "tstack.h"
 
+template<class T>
+class TStack {
+  private:
+    T arr[size];
+    int top;
+  public:
+    TStack():top(-1) { }
+    T get() const { 
+      return arr[top];
+    }
+    bool isEmpty() const {
+      return top==-1;
+    }
+    bool isFull() const {
+      return top==size-1;
+    }
+    void pop() {
+      if(top>=0)
+        top--;
+    }
+    void push(T item) {
+      if(top<size-1)
+        arr[++top]=item;
+     }
+};
+
+
 int prior(char c) {
   if (c == '(') {
     return 0;
