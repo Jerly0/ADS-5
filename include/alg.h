0b4184540b1d4b3e45a4a06ac98b01a310fe453d
Jerly0 <87529148+Jerly0@users.noreply.github.com>
2022-04-02 14:35:34 +0300

    Update alg.h

--- include/alg.h
+++ include/alg.h
@@ -20,18 +20,18 @@ class TStack {
         return arr[top];
      }
      bool isEmpty() const {
-        return top==-1;
+        return top == -1;
      }
      bool isFull() const {
-        return top==size-1;
+        return top == size - 1;
      }
      void pop() {
-        if(top>=0)
+        if(top >= 0)
           top--;
      }
      void push(T item) {
-       if(top<size-1)
-         arr[++top]=item;
+       if(top < size - 1)
+         arr[++top] = item;
      }
 };
 
