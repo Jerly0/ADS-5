77e185dbe98a3a852690d9b211f3a23a0e2c9ed5
Jerly0 <87529148+Jerly0@users.noreply.github.com>
2022-04-02 12:58:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,21 +4,21 @@
 #include "tstack.h"
 
 int prior(char c) {
-  if(c == '('){
-    return 0;
-  }
-  else if (c == ')') {
-    return 1;
-  }
-  else if ((c ==  '+') || (c == '-')) {
-    return 2;
-  }
-  else if ((c == '*') || (c == '/')) {
-    return 3;
-  }
-  else {
-    return -1;
-  }
+ if (c == '(') {
+   return 0;
+ }
+ else if (c == ')') {
+   return 1;
+ }
+ else if ((c ==  '+') || (c == '-')) {
+   return 2;
+ }
+ else if ((c == '*') || (c == '/')) {
+   return 3;
+ }
+ else {
+   return -1;
+ }
 }
 
 std::string infx2pstfx(std::string inf) {
