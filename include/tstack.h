eb8d6c738038ba0e54209d082b71ebae6f214152
Jerly0 <87529148+Jerly0@users.noreply.github.com>
2022-04-02 14:40:18 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,8 +1,32 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <iostream>
+const int size = 100;
 
 template<typename T, int size>
 class TStack {
+    private:
+      T arr[size];
+      int top;
+    public:
+      TStack():top(-1) { }
+      T get() const { 
+         return arr[top];
+     }
+     bool isEmpty() const {
+        return top == -1;
+     }
+     bool isFull() const {
+        return top == size - 1;
+     }
+     void pop() {
+        if(top >= 0)
+          top--;
+     }
+     void push(T item) {
+       if(top < size - 1)
+         arr[++top] = item;
+     }
 };
 #endif  // INCLUDE_TSTACK_H_
